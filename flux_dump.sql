ddb22bf29941851857643cf34b6a1f743c888440
yuriikostyshyn <yuriywert@gmail.com>
2012-07-01 23:48:07 +0300

    Create datamodel for BankMockApp https://www.pivotaltracker.com/story/show/22161469
    Amended dump file for flux app and added dump for bank mock app.

--- flux_dump.sql
+++ flux_dump.sql
@@ -1,12 +1,20 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+/*
+Navicat MySQL Data Transfer
 
-DROP SCHEMA IF EXISTS `flux` ;
-CREATE SCHEMA IF NOT EXISTS `flux` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `flux` ;
+Source Server         : flux
+Source Server Version : 50524
+Source Host           : localhost:3306
+Source Database       : flux
+
+Target Server Type    : MYSQL
+Target Server Version : 50524
+File Encoding         : 65001
+
+Date: 2012-07-01 23:41:13
+*/
 
 SET FOREIGN_KEY_CHECKS=0;
+
 -- ----------------------------
 -- Table structure for `account`
 -- ----------------------------
@@ -16,7 +24,6 @@ CREATE TABLE `account` (
   `user_id` int(11) DEFAULT NULL,
   `bank_id` int(11) DEFAULT NULL,
   `currency_id` int(11) DEFAULT NULL,
-  `amount` double(20,0) DEFAULT NULL,
   `security_key` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`account_id`),
   KEY `account_ibfk_1` (`user_id`),
@@ -28,7 +35,7 @@ CREATE TABLE `account` (
 -- ----------------------------
 -- Records of account
 -- ----------------------------
-INSERT INTO `account` VALUES ('1', '1', '1', '1', '123', '12133');
+INSERT INTO `account` VALUES ('1', '1', '1', '1', '12133');
 
 -- ----------------------------
 -- Table structure for `currency`
@@ -47,48 +54,6 @@ CREATE TABLE `currency` (
 INSERT INTO `currency` VALUES ('1', 'UAH', 'Ukrainian hryvnia');
 
 -- ----------------------------
--- Table structure for `status`
--- ----------------------------
-DROP TABLE IF EXISTS `status`;
-CREATE TABLE `status` (
-  `status_id` int(11) NOT NULL DEFAULT '0',
-  `status_message` varchar(30) CHARACTER SET utf8 DEFAULT NULL,
-  PRIMARY KEY (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of status
--- ----------------------------
-
--- ----------------------------
--- Table structure for `transaction`
--- ----------------------------
-DROP TABLE IF EXISTS `transaction`;
-CREATE TABLE `transaction` (
-  `transaction_id` bigint(20) NOT NULL DEFAULT '0',
-  `account_id_from` bigint(20) DEFAULT NULL,
-  `account_id_to` bigint(20) DEFAULT NULL,
-  `start_date` timestamp NULL DEFAULT NULL,
-  `end_date` timestamp NULL DEFAULT NULL,
-  `amount` double(20,0) DEFAULT NULL,
-  `status_id` int(11) DEFAULT NULL,
-  `currency_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`transaction_id`),
-  KEY `account_id_from` (`account_id_from`),
-  KEY `account_id_to` (`account_id_to`),
-  KEY `status_id` (`status_id`),
-  KEY `currency_id` (`currency_id`),
-  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`account_id_from`) REFERENCES `account` (`account_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`account_id_to`) REFERENCES `account` (`account_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of transaction
--- ----------------------------
-
--- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -100,9 +65,10 @@ CREATE TABLE `user` (
   `surname` varchar(30) CHARACTER SET utf8 DEFAULT NULL,
   `security_key` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES ('1', 'flux@flux.com', 'Flux1', 'Flux', 'Flux', '0');
+INSERT INTO `user` VALUES ('2', 'yuriywert@gmail.com', 'password', 'Yurii', 'Kostyshyn', '1341342332e23242');
